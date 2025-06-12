c0cfeb9973e2fd92d3f058bb285259c81def5bf2
noraainuse <nxx4235@gmail.com>
2025-06-12 12:44:40 +0700

    beforenoon: day4

--- onsite/day4/beforenoon/premium.py
+++ onsite/day4/beforenoon/premium.py
@@ -1,6 +1,6 @@
 '''premium'''
 def main():
     '''main'''
-    x = 'input() + ' '
-    x.replace("Ads", "Kreem Boos Boos")
+    x = ' ' + input() + ' '
+    print(x.replace(" Ads ", " Kreem Boos Boos ").strip())
 main()
